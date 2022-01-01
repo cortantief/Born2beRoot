96003bb73307201e857338d056ac1e77d925d71d
cortantief <harboulisoufiane@protonmail.com>
2022-01-01 06:41:57 +0100

    We use wall now to send message to all logges user

--- monitoring.sh
+++ monitoring.sh
@@ -1,9 +1,12 @@
+#!/bin/bash
+
+FILEO=$(mktemp)
+
 print_message () {
-	echo "	#$1:" $2
+	echo "    #$1:" $2 >> $FILEO
 }
 
-echo "Brodcast message from root@$HOSTNAME:"
-echo ""
+SUDO_SEQ_FILE="/var/log/sudo/seq"
 
 print_message "Architecture" "$(uname -a)"
 
@@ -19,12 +22,12 @@ FREE_RAM=$(echo $FREE_IN_MB | cut -d ' ' -f 3)
 PERCENT_RAM=$(awk -v FREE_RAM="$FREE_RAM" -v USED_RAM="$USED_RAM" 'BEGIN{ printf "%.2f\n", (USED_RAM / FREE_RAM) * 100 }')
 print_message "Memory Usage (MB)" "$USED_RAM/$FREE_RAM ($PERCENT_RAM%)"
 
-DISK_IN_GB=$(df -BG | grep "/$" | xargs)
+DISK_IN_GB=$(df -BM | grep "/$" | xargs)
 
 PERC_DISK=$(echo $DISK_IN_GB | cut -d ' ' -f 5)
 USED_DISK=$(echo $DISK_IN_GB | cut -d ' ' -f 3 | grep "[[:digit:]]\+" -o)
 AVA_DISK=$(echo $DISK_IN_GB | cut -d ' ' -f 2 | grep "[[:digit:]]\+" -o)
-print_message "Disk Usage (GB)" "$USED_DISK/$AVA_DISK ($PERC_DISK)"
+print_message "Disk Usage (MB)" "$USED_DISK/$AVA_DISK ($PERC_DISK)"
 print_message "CPU load" "$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {printf "%.2f", usage}')%"
 
 
@@ -32,8 +35,19 @@ BOOT_OP=$(who -b | xargs)
 
 print_message "Last boot" "$(echo $BOOT_OP | cut -d ' ' -f 3) $(echo $BOOT_OP | cut -d ' ' -f 4)"
 
-print_message "LVM use" "$(if $(cat /etc/fstab | grep '/dev/mapper/'); then echo 'yes';else echo 'no'; fi)"
+
+print_message "LVM use" "$(if grep -F '/dev/mapper/' /etc/fstab &>/dev/null ; then echo 'yes';else echo 'no'; fi)"
 
 print_message "Connexions TCP" "$(ss -ant | grep ESTAB| wc -l)"
+print_message "User log" "$(users | wc -w)"
+
 IP_ADDR=$(hostname -I |cut -d ' ' -f 1)
 print_message "Network" "$IP_ADDR ($(ip addr show | grep $IP_ADDR -B 1 | head -n 1 | xargs | cut -d ' ' -f 2))"
+
+
+# https://stackoverflow.com/questions/4614775/converting-hex-to-decimal-in-awk-or-sed/32437561
+
+print_message "Sudo" "$(cat $SUDO_SEQ_FILE | awk -Wposix '{printf("%d\n","0x" $1)}') cmd"
+
+wall < $FILEO
+rm -rf $FILEO
