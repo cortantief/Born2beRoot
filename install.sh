d0c6836342f623f497d487e46fe79ac80c7b8d99
Soufiane HARBOULI <sharboul@e3r2p4.42.fr>
2022-01-03 10:07:26 +0100

    Complteting scripts to perform installation of all configuration files

--- install.sh
+++ install.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+
+USERNAME="sharboul"
+SCRIPT_PATH="/root/.scripts/"
+
 apt-get update -y
 apt upgrade -y
 apt-get install -y libpam-pwquality libpwquality-tools sudo
+groupadd user42
+usermod -a -G user42,sudo $USERNAME
+mkdir -p $SCRIPT_PATH
+
+cp monitoring.sh > $SCRIPT_PATH
+cat login.defs > /etc/login.defs
+cat common-password > /etc/pam.d/common-password
+cat sshd_config > /etc/ssh/sshd_config
+cat crontab > /etc/crontab
+cat sudoers > /etc/sudoers
+
