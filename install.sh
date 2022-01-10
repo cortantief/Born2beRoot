180b2d29bf213f20ca041e02b486fae997b0cf97
Soufiane HARBOULI <sharboul@e3r2p6.42.fr>
2022-01-10 17:07:04 +0100

    Finishing install file by adding ufw with correct rules

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ SCRIPT_PATH="/root/.scripts/"
 
 apt-get update -y
 apt upgrade -y
-apt-get install -y libpam-pwquality libpwquality-tools sudo
+apt-get install -y libpam-pwquality libpwquality-tools sudo ufw
 groupadd user42
 usermod -a -G user42,sudo $USERNAME
 mkdir -p $SCRIPT_PATH
@@ -17,3 +17,7 @@ cat sshd_config > /etc/ssh/sshd_config
 cat crontab > /etc/crontab
 cat sudoers > /etc/sudoers
 
+ufw default deny incoming
+ufw allow 4242
+ufw enable
+systemctl restart sshd
